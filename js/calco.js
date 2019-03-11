4842878818781381bf40d8b452217371552a6178
koffi09 <45895139+koffi09@users.noreply.github.com>
2019-03-11 19:57:52 +0100

    Add files via upload

---
+++ js/calco.js
@@ -0,0 +1,139 @@
+function un()
+{
+
+var x= document.getElementById("un").value;
+var valeur =document.getElementById("resultat").value;
+document.getElementById("resultat").value= valeur+x;
+
+}
+
+ function deux()
+ {
+   var x=document.getElementById('deux').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+
+
+function trois()
+{
+
+var x= document.getElementById("trois").value;
+var valeur =document.getElementById("resultat").value;
+document.getElementById("resultat").value= valeur+x;
+
+}
+
+ function quatre()
+ {
+   var x=document.getElementById('quatre').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+
+function cinq()
+{
+
+var x= document.getElementById("cinq").value;
+var valeur =document.getElementById("resultat").value;
+document.getElementById("resultat").value= valeur+x;
+
+}
+
+ function six()
+ {
+   var x=document.getElementById('six').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+function sept()
+{
+
+var x= document.getElementById("sept").value;
+var valeur =document.getElementById("resultat").value;
+document.getElementById("resultat").value= valeur+x;
+
+}
+
+ function huit()
+ {
+   var x=document.getElementById('huit').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+function neuf()
+{
+
+var x= document.getElementById("neuf").value;
+var valeur =document.getElementById("resultat").value;
+document.getElementById("resultat").value= valeur+x;
+
+}
+
+ function zero()
+ {
+   var x=document.getElementById('zero').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+function plus()
+{
+
+var x= document.getElementById("plus").value;
+var valeur =document.getElementById("resultat").value;
+document.getElementById("resultat").value= valeur+x;
+
+}
+
+ function egal()
+ {
+  var valeur=document.getElementById("resultat").value;
+  var resultatFinal=eval(valeur);
+  document.getElementById("resultat").value= resultatFinal;
+  
+ }
+ 
+ function point()
+ {
+   var x=document.getElementById('point').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+
+  function moin()
+ {
+   var x=document.getElementById('moin').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+
+ function slash ()
+ {
+   var x=document.getElementById('slash').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+
+ function multi ()
+ {
+   var x=document.getElementById('multi').value;
+   var valeur=document.getElementById('resultat').value;
+  document.getElementById('resultat').value= valeur+x;
+ }
+
+
+ function suppr ()
+ {
+   var valeur =document.getElementById('resultat').value;
+   var longueur=valeur.length;
+    var fin=longueur-1;
+    valeur=valeur.substr(0,fin);
+   document.getElementById('resultat').value= valeur;
+ }
+
+ function modulo()
+ {
+   var x =document.getElementById('modulo').value;
+   var valeur=document.getElementById('resultat').value;
+   document.getElementById('resultat').value= valeur+x;
+ }
